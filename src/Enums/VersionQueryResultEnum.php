fb78457694452f6a57a8842bf2a87fb48f784280
Ruslan Gataullin <fisher7410@yandex.ru>
2020-03-07 03:45:44 +0500

    Added change enum

---
+++ src/Enums/VersionQueryResultEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Solid\VersionChecker\Enums;
+
+class VersionQueryResultEnum
+{
+    const MAJOR_CHANGE = 'major';
+    const MINOR_CHANGE = 'minor';
+    const BUGFIX = 'fix';
+}
